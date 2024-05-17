048de27b3aaa2fb916afe5c5b6ba8c965b9ab26c
nzommmo <ericnzomo17@gmail.com>
2024-05-17 22:52:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
- 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
- 
-    </div>
+    <>
+    <navbar/>
+    </>
   )
 }
 
